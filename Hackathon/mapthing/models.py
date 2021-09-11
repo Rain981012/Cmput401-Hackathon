455f356fd6028e9f373dd57c1b3419bfdafc65e8
Andrews Essilfie <essilfie@ualberta.ca>
2021-09-11 14:11:22 -0600

    fixed django user - used django user instead

--- Hackathon/mapthing/models.py
+++ Hackathon/mapthing/models.py
@@ -1,12 +1,7 @@
 from django.db import models
 from django.db.models.deletion import CASCADE
 from django.utils import timezone
-
-class User(models.Model):
-    user_name = models.CharField(max_length=70)
-    
-    def __str__(self):
-        return self.user_name
+from django.contrib.auth.models import User
 
 class Event(models.Model):
     event_name = models.CharField(max_length=200)
