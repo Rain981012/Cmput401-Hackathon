5b4f793d3de7989bc3438183b4f900ff70d67135
Andrews Essilfie <essilfie@ualberta.ca>
2021-09-11 12:45:09 -0600

    fixed issues

--- Hackathon/mapthing/models.py
+++ Hackathon/mapthing/models.py
@@ -1,16 +1,24 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 from django.utils import timezone
+
+class User(models.Model):
+    user_name = models.CharField(max_length=70)
+    
+    def __str__(self):
+        return self.user_name
 
 class Event(models.Model):
     event_name = models.CharField(max_length=200)
-    creator = models.CharField(max_length=100)
+    creator = models.ForeignKey(User, on_delete=CASCADE)
     description = models.CharField(max_length=400)
     longitude = models.FloatField()
     latitude = models.FloatField()
     start_date = models.DateTimeField('published date')
     end_date = models.DateTimeField('end date')
-    attendants = models.IntegerField(default=0)
-    visibility = models.BooleanField(default=True)
+    min_number_people = models.IntegerField(null=True)
+    max_number_people = models.IntegerField(null=True)
+    public = models.BooleanField(default=True)
     
     def __str__(self) -> str:
         return self.event_name
